6f048705f901c6c27a98d69511ebe490755e947c
Zeheng Li <imzehengl@gmail.com>
2016-11-13 20:37:29 -0700

    update app

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, send_file, request
 import qrcode
-import StringIO
+from StringIO import StringIO
 
 app = Flask(__name__)
 
 
 def serve_image(img):
-    img_io = StringIO.StringIO()
+    img_io = StringIO()
     img.save(img_io, 'png')
     img_io.seek(0)
     return send_file(img_io, mimetype='image/png')
@@ -15,7 +15,6 @@ def serve_image(img):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print request.form['password']
         img = qrcode.make(request.form['password'])
         return serve_image(img)
     else:
@@ -23,24 +22,21 @@ def index():
         <!DOCTYPE html>
         <html lang="en">
           <head>
-            <!-- Required meta tags always come first -->
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
             <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-            <!-- Bootstrap CSS -->
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
           </head>
           <body>
             <form method="post">
                 <div class="form-group">
-                    <label for="exampleInputEmail1">wifi password:</label>
-                    <input type="text" name="password" class="form-control" placeholder="Enter wifi password">
+                    <label for="exampleInputEmail1">String to convert:</label>
+                    <input type="text" name="password" class="form-control" placeholder="Type Something">
                     <input type="submit" value="Submit">
                 </div>              
             </form>
 
-            <!-- jQuery first, then Tether, then Bootstrap JS. -->
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
