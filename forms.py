f88040788fe40af7d2a2530c06eb71912afa7cbe
Zeheng Li <imzehengl@gmail.com>
2021-11-08 00:57:39 -0700

    refresh deps

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextField
+from wtforms import SubmitField, StringField
 from wtforms.validators import DataRequired
 
 
 class TextInputForm(FlaskForm):
-    text_input = TextField("Text Input", validators=[DataRequired()])
+    text_input = StringField("Text Input", validators=[DataRequired()])
     submit = SubmitField("Submit")
